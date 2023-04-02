3b85c7d79ab854b5c9a6d931aba0339eeb16b82e
sweetburble <79733289+sweetburble@users.noreply.github.com>
2023-04-02 20:41:20 +0900

    Programmers_131116 정태현 - Add 1st Solution

---
+++ 정태현/4주차/131116.sql
@@ -0,0 +1,7 @@
+SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
+FROM FOOD_PRODUCT
+WHERE (CATEGORY, PRICE) IN (SELECT CATEGORY, MAX(PRICE)
+                            FROM FOOD_PRODUCT
+                            WHERE CATEGORY IN ('과자', '국', '김치', '식용유')
+                            GROUP BY CATEGORY)
+ORDER BY PRICE DESC;
